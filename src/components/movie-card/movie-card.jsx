3246ad02e1a67175cb2b886a5f32d1a482a1acd2
Doris24 <33037450+Doris24@users.noreply.github.com>
2021-11-19 01:21:53 +0100

    Imported bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types'; //PropTypes validate data types based on the app's configuration
 
+//Bootstrap
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+
+    return (
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} cariant="link">Open</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 
